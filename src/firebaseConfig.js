2c2d091c01841946c6dbf6290d444e7b859f350d
Anirudhya Duyti <adchinmoy@gmail.com>
2021-03-15 21:22:09 +0600

    firebase init

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC_gmS2tIaTSeFTWUq4aCKjWVysN0Rj2Zs",
+    authDomain: "auto-demo-6df1e.firebaseapp.com",
+    projectId: "auto-demo-6df1e",
+    storageBucket: "auto-demo-6df1e.appspot.com",
+    messagingSenderId: "1033329024616",
+    appId: "1:1033329024616:web:8dbb0f68ef808ba600e5dd"
+};
+
+export default firebaseConfig;
